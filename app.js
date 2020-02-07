c2df2095c4d15342d80077b8790e893e305505ee
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-07 13:59:24 +1100

    [update] Routing

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('./src/db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+const userRouter = require('./routes/user')
+const taskRouter = require('./routes/task')
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server has started!')
+})
