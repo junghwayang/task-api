370b835d7ed83412f14de7377b70231d3b2b9557
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-15 21:42:16 +1100

    [modify] Delete useless import

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 require('./src/db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routes/user')
 const taskRouter = require('./routes/task')
 
