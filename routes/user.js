b1655d9548c053898db9ccb6373af15a26158cfc
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-22 23:11:38 +1100

    [update] File uploads

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 
@@ -98,6 +99,15 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+
+// Upload avatar
+const upload = multer({
+    dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
 })
 
 module.exports = router
