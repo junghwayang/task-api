ebac96f879c42c5b078406df01b820ff29fd0a84
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-25 17:34:24 +1100

    [update] Deleting avatar routes

--- routes/user.js
+++ routes/user.js
@@ -103,7 +103,6 @@ router.delete('/users/me', auth, async (req, res) => {
 
 // Upload avatar
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -116,10 +115,19 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
+})
+
+// Delete avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
