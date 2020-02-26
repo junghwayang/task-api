c0792b72a0233af1b2b2e7a379a6597c66b2119d
Junghwa Yang <rosie.junghwa@gmail.com>
2020-02-26 15:25:54 +1100

    [update] Read avatar

--- routes/user.js
+++ routes/user.js
@@ -130,4 +130,20 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send()
 })
 
+// Read avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
 module.exports = router
