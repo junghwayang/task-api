4c49274294397b4804af34d7a1fbd111d5f1800c
Junghwa Yang <rosie.junghwa@gmail.com>
2020-03-19 00:47:16 +1100

    Environment config using env-cmd

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
